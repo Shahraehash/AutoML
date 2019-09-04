fcee216cdbd1b6feb0577759bc189aedff7417e6
Samer Albahra <salbahra@iMac.localdomain>
2019-09-04 12:11:31 -0700

    Add 50% select percentile

--- feature_selection.py
+++ feature_selection.py
@@ -3,10 +3,12 @@ from sklearn.feature_selection import SelectPercentile
 
 featureSelectors = {
     'none': '',
+    'select-50': SelectPercentile(percentile=50),
     'select-75': SelectPercentile(percentile=75)
 }
 
 featureSelectorNames = {
     'none': 'no feature selection',
+    'select-50': 'select percentile (50%)',
     'select-75': 'select percentile (75%)'
 }
