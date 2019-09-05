5939d36329d04529dc84284cbf87df6e4a1a74b0
Samer Albahra <salbahra@gmail.com>
2019-09-04 19:32:20 -0700

    Clean imported data (remove non-numeric input and show negative/positive ratio)

--- import_data.py
+++ import_data.py
@@ -1,5 +1,6 @@
 # Dependencies
 import pandas as pd
+import numpy as np
 
 from sklearn.model_selection import train_test_split
 
@@ -12,6 +13,13 @@ def importData(trainPath, testPath, labelColumn):
     X2 = data_test.drop(labelColumn, axis=1)
     Y2 = data_test[labelColumn]
 
+    # Only keep numeric inputs
+    X = X.loc[:, (X.dtypes == np.int64) | (X.dtypes == np.float64)].dropna()
+    X2 = X2.loc[:, (X2.dtypes == np.int64) | (X2.dtypes == np.float64)].dropna()
+
+    classRatio = data[data[labelColumn] == 0].shape[0] / data[data[labelColumn] == 1].shape[0]
+
+    print('Ratio of negative to positive conditions (ideal is 1): %.7g' % classRatio)
+
     # Generate test/train split from the train data
     return [data, data_test, X, Y, X2, Y2] + train_test_split(X, Y, test_size=.2, random_state=5, stratify=Y)
-
