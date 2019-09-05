3db7c612ed472fc1b93f425a80bdbb63dabe0ceb
Samer Albahra <salbahra@Samers-MacBook-Air.local>
2019-09-05 12:59:25 -0700

    Imporve output when importing data to show totals for both cases and show warning if unbalanced

--- import_data.py
+++ import_data.py
@@ -17,9 +17,13 @@ def importData(trainPath, testPath, labelColumn):
     X = X.loc[:, (X.dtypes == np.int64) | (X.dtypes == np.float64)].dropna()
     X2 = X2.loc[:, (X2.dtypes == np.int64) | (X2.dtypes == np.float64)].dropna()
 
-    classRatio = data[data[labelColumn] == 0].shape[0] / data[data[labelColumn] == 1].shape[0]
+    negativeCount = data[data[labelColumn] == 0].shape[0]
+    positiveCount = data[data[labelColumn] == 1].shape[0]
 
-    print('Ratio of negative to positive conditions (ideal is 1): %.7g' % classRatio)
+    print('Negative Cases: %.7g\nPositive Cases: %.7g' % (negativeCount, positiveCount))
+
+    if negativeCount / positiveCount < .9:
+        print('Warning: Classes are not balanced.')
 
     # Generate test/train split from the train data
     return [data, data_test, X, Y, X2, Y2] + train_test_split(X, Y, test_size=.2, random_state=5, stratify=Y)
