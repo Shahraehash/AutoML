454ddb7594107325cec9c6cfc2e2de5a5c8b2873
Samer Albahra <salbahra@gmail.com>
2019-09-04 20:33:30 -0700

    Add the ability to bypass estimators, feature selectors and scalers with a environment file

--- app.py
+++ app.py
@@ -5,13 +5,11 @@
 # and hyper-parameters with feature engineering.
 
 #%%
-# Hide warning from the output
-import warnings
-warnings.filterwarnings('ignore')
-
-#%%
 # Dependencies
+import os
 import itertools
+import warnings
+from dotenv import load_dotenv
 
 from estimators import estimatorNames
 from feature_selection import featureSelectorNames
@@ -19,6 +17,17 @@ from model import generateModel
 from import_data import importData
 from pipeline import generatePipeline
 from scalers import scalerNames
+
+#%%
+# Load environment variables
+load_dotenv()
+IGNORE_ESTIMATOR = os.getenv('IGNORE_ESTIMATOR').split(',')
+IGNORE_FEATURE_SELECTOR = os.getenv('IGNORE_FEATURE_SELECTOR').split(',')
+IGNORE_SCALER = os.getenv('IGNORE_SCALER').split(',')
+
+#%%
+# Hide warning from the output
+warnings.filterwarnings('ignore')
 
 #%%
 # Define the labels for our classes
@@ -34,7 +43,8 @@ data, data_test, X, Y, X2, Y2, X_train, X_test, Y_train, Y_test = importData('da
 # Generate all models
 models = {}
 for estimator, featureSelector, scaler in list(itertools.product(*[estimatorNames, featureSelectorNames, scalerNames])):
-    print('Generating ' + estimatorNames[estimator] + ' model with ' + scalerNames[scaler] + ' and with ' + featureSelectorNames[featureSelector])
+    if estimator in IGNORE_ESTIMATOR or featureSelector in IGNORE_FEATURE_SELECTOR or scaler in IGNORE_SCALER:
+        continue
 
     if not scaler in models:
         models[scaler] = {}
@@ -42,8 +52,6 @@ for estimator, featureSelector, scaler in list(itertools.product(*[estimatorName
     if not featureSelector in models[scaler]:
         models[scaler][featureSelector] = {}
 
+    print('Generating ' + estimatorNames[estimator] + ' model with ' + scalerNames[scaler] + ' and with ' + featureSelectorNames[featureSelector])
     pipeline = generatePipeline(scaler, featureSelector, estimator)
     models[scaler][featureSelector][estimator] = generateModel(estimator, pipeline, X_train, Y_train, X, Y, X2, Y2, labels)
-
-
-#%%
