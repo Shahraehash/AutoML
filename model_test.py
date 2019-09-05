c762413c38b9e7dec760fedc3e0d5516bc13c61a
Samer Albahra <salbahra@Samers-MacBook-Air.local>
2019-09-05 12:52:04 -0700

    Fix unit tests to use weighted F1 scores

--- model_test.py
+++ model_test.py
@@ -13,7 +13,7 @@ def test_logistic_regression():
     model = generateModel('lr', pipeline, X_train, Y_train, X, Y, X2, Y2)
     assert model['generalization']['accuracy'] == 0.37254901960784315
     assert model['generalization']['auc'] == 0.6
-    assert model['generalization']['f1'] == 0.40740740740740744
+    assert model['generalization']['f1'] == 0.3703703703703704
     assert model['generalization']['sensitivity'] == 1.0
     assert model['generalization']['specificity'] == 0.2
 
@@ -22,7 +22,7 @@ def test_logistic_regression_with_standard_scaler():
     model = generateModel('lr', pipeline, X_train, Y_train, X, Y, X2, Y2)
     assert model['generalization']['accuracy'] == 0.49019607843137253
     assert model['generalization']['auc'] == 0.675
-    assert model['generalization']['f1'] == 0.4583333333333333
+    assert model['generalization']['f1'] == 0.48842592592592593
     assert model['generalization']['sensitivity'] == 1.0
     assert model['generalization']['specificity'] == 0.35
 
@@ -31,7 +31,7 @@ def test_k_nearest_neighbor():
     model = generateModel('knn', pipeline, X_train, Y_train, X, Y, X2, Y2)
     assert model['generalization']['accuracy'] == 0.39215686274509803
     assert model['generalization']['auc'] == 0.6125
-    assert model['generalization']['f1'] == 0.41509433962264153
+    assert model['generalization']['f1'] == 0.39122063919907585
     assert model['generalization']['sensitivity'] == 1.0
     assert model['generalization']['specificity'] == 0.225
 
@@ -40,7 +40,7 @@ def test_k_nearest_neighbor_with_standard_scaler():
     model = generateModel('knn', pipeline, X_train, Y_train, X, Y, X2, Y2)
     assert model['generalization']['accuracy'] == 0.5490196078431373
     assert model['generalization']['auc'] == 0.6795454545454545
-    assert model['generalization']['f1'] == 0.4651162790697674
+    assert model['generalization']['f1'] == 0.5376428852975955
     assert model['generalization']['sensitivity'] == 0.9090909090909091
     assert model['generalization']['specificity'] == 0.45
 
@@ -49,7 +49,7 @@ def test_support_vector_machine():
     model = generateModel('svm', pipeline, X_train, Y_train, X, Y, X2, Y2)
     assert model['generalization']['accuracy'] == 0.2549019607843137
     assert model['generalization']['auc'] == 0.4920454545454545
-    assert model['generalization']['f1'] == 0.3448275862068965
+    assert model['generalization']['f1'] == 0.24059561128526644
     assert model['generalization']['sensitivity'] == 0.9090909090909091
     assert model['generalization']['specificity'] == 0.075
 
@@ -58,6 +58,6 @@ def test_support_vector_machine_with_standard_scaler():
     model = generateModel('svm', pipeline, X_train, Y_train, X, Y, X2, Y2)
     assert model['generalization']['accuracy'] == 0.4117647058823529
     assert model['generalization']['auc'] == 0.625
-    assert model['generalization']['f1'] == 0.4230769230769231 
+    assert model['generalization']['f1'] == 0.4115384615384615 
     assert model['generalization']['sensitivity'] == 1.0
     assert model['generalization']['specificity'] == 0.25
