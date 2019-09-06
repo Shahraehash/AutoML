f33f6b35ff07217659bf426db28f96c73f4972e3
Samer Albahra <salbahra@gmail.com>
2019-09-05 21:58:47 -0700

    Fix pipeline to properly use selectors

---
+++ generalization.py
@@ -0,0 +1,43 @@
+# Dependencies
+from sklearn.metrics import roc_auc_score, accuracy_score, confusion_matrix, classification_report, f1_score
+
+from scalers import scalers
+
+def generalize(model, scaler, X_train, X2, Y2, labels):
+
+    # Identify the selected featured for the above pipeline
+    for feature, selected in model['selected_features'].items():
+        if not selected:
+            X_train = X_train.drop(feature, axis=1)
+            X2 = X2.drop(feature, axis=1)
+
+    sc = scalers[scaler]
+    sc.fit(X_train)
+    X2 = sc.transform(X2)
+
+    predictions = model['best_estimator'].predict(X2)
+    print('\t', classification_report(Y2, predictions, target_names=labels).replace('\n', '\n\t'))
+
+    print('\tGeneralization:')
+
+    accuracy = accuracy_score(Y2, predictions)
+    print('\t\tAccuracy:', accuracy)
+
+    auc = roc_auc_score(Y2, predictions)
+    print('\t\tAUC:', auc)
+
+    tn, fp, fn, tp = confusion_matrix(Y2, predictions).ravel()
+    f1 = f1_score(Y2, predictions, average='macro')
+    sensitivity = tp / (tp+fn)
+    specificity = tn / (tn+fp)
+    print('\t\tSensitivity:', sensitivity)
+    print('\t\tSpecificity:', specificity)
+    print('\t\tF1:', f1, '\n')
+
+    return {
+        'accuracy': accuracy,
+        'auc': auc,
+        'f1': f1,
+        'sensitivity': sensitivity,
+        'specificity': specificity
+    }
