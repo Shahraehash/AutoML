7a6dbd994ab95c1c4d0f2a827cc899c334e57571
Samer Albahra <salbahra@MacBook-Air.localdomain>
2019-09-11 13:46:29 -0700

    Add 25% option to all feature selectors

--- feature_selection.py
+++ feature_selection.py
@@ -6,20 +6,26 @@ from random_forest_importance_select import RandomForestImportanceSelect
 
 featureSelectors = {
     'none': '',
+    'pca-25': PCA(n_components=.25, svd_solver='full'),
     'pca-50': PCA(n_components=.50, svd_solver='full'),
     'pca-75': PCA(n_components=.75, svd_solver='full'),
+    'rf-25': RandomForestImportanceSelect(n=.25),
     'rf-50': RandomForestImportanceSelect(n=.50),
     'rf-75': RandomForestImportanceSelect(n=.75),
+    'select-25': SelectPercentile(percentile=25),
     'select-50': SelectPercentile(percentile=50),
     'select-75': SelectPercentile(percentile=75)
 }
 
 featureSelectorNames = {
     'none': 'all features (no feature selection)',
+    'pca-25': 'principal component analysis (25%)',
     'pca-50': 'principal component analysis (50%)',
     'pca-75': 'principal component analysis (75%)',
+    'rf-25': 'random forest importance (25%)',
     'rf-50': 'random forest importance (50%)',
     'rf-75': 'random forest importance (75%)',
+    'select-25': 'select percentile (25%)',
     'select-50': 'select percentile (50%)',
     'select-75': 'select percentile (75%)'
 }
