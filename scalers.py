18d2d421670c3752ca896d33ea1c2ac1b9fa5fff
Samer Albahra <salbahra@gmail.com>
2019-09-10 19:49:58 -0700

    Add min max scaler option

--- scalers.py
+++ scalers.py
@@ -1,12 +1,14 @@
 # Dependencies
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
 scalers = {
     'none': '',
+    'minmax': MinMaxScaler(),
     'std': StandardScaler()
 }
 
 scalerNames = {
     'none': 'no scaling',
+    'minmax': 'min max scaler',
     'std': 'standard scaler'
 }
