617d12eb49eaa1bab0a98de7c1b21b93c0be712b
Samer Albahra <salbahra@MacBook-Air.localdomain>
2019-09-11 20:15:18 -0700

    Improve pylint results

--- operators/debug.py
+++ operators/debug.py
@@ -7,12 +7,14 @@ from sklearn.base import TransformerMixin, BaseEstimator
 class Debug(BaseEstimator, TransformerMixin):
     """Base class"""
 
+    #pylint: disable = no-self-use
     def transform(self, x):
         """Show data passing through"""
 
         print('\tNumber of items: %d\n\tNumber of features: %d' % (x.shape[0], x.shape[1]))
         return x
 
+    #pylint: disable = unused-argument
     def fit(self, x, y=None, **fit_params):
         """Do nothing"""
 
