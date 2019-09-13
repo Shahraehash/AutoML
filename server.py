95ef168f2e3d5f05b79b3bf71c075b3d4ee1a9e3
Samer Albahra <salbahra@gmail.com>
2019-09-12 19:35:05 -0700

    Add working upload

--- server.py
+++ server.py
@@ -16,19 +16,36 @@ APP = Flask(__name__, static_url_path='')
 def load_ui():
     return send_from_directory('static', 'index.html')
 
+@APP.route('/train', methods=['POST'])
+def run():
+    body = request.get_json()
+    results = api.find_best_model(body['train'], body['test'], body['labels'], body['label_column'])
+    return jsonify(results)
+
+@APP.route('/upload', methods=['POST'])
+def upload_files():
+    print(request.files)
+    if 'train' not in request.files or 'test' not in request.files:
+        return jsonify({'error': 'Missing files'})
+
+    train = request.files['train']
+    test = request.files['test']
+    if train.filename != 'train.csv' or test.filename != 'test.csv':
+        return jsonify({'error': 'Missing files'})
+
+    if train and test:
+        train.save('data/' + train.filename)
+        test.save('data/' + test.filename)
+        return jsonify({'success': 'true'})
+
+    return jsonify({'error': 'unknown'})
+
 @APP.route('/<path:path>')
 def get_static_file(path):
     if not os.path.isfile(os.path.join('static', path)):
         path = os.path.join(path, 'index.html')
 
     return send_from_directory('static', path)
-
-@APP.route('/train', methods=['POST'])
-def run():
-    body = request.get_json()
-    print(body)
-    results = api.find_best_model(body['train'], body['test'], body['labels'], body['label_column'])
-    return jsonify(results)
 
 if __name__ == "__main__":
     APP.run()
--- ui/src/app/home/home.page.ts
+++ ui/src/app/home/home.page.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  SERVER_URL = 'http://localhost:5000/upload';
+  uploadForm: FormGroup;
 
-  constructor() {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private httpClient: HttpClient
+  ) {}
 
+  ngOnInit() {
+    this.uploadForm = this.formBuilder.group({
+      label_column: '',
+      train: [''],
+      test: ['']
+    });
+  }
+
+  onSubmit() {
+    const formData = new FormData();
+    formData.append('train', this.uploadForm.get('train').value);
+    formData.append('test', this.uploadForm.get('test').value);
+
+    this.httpClient.post<any>(this.SERVER_URL, formData).subscribe(
+      (res) => console.log(res),
+      (err) => console.log(err)
+    );
+
+    return false;
+  }
+
+  onFileSelect(event) {
+    if (event.target.files.length === 1) {
+      const file = event.target.files[0];
+      this.uploadForm.get(event.target.name).setValue(file);
+    }
+  }
 }
