b337c0b65e1bdd9d857e53afc4cdbfa12dd5225f
Samer Albahra <salbahra@gmail.com>
2019-09-17 15:44:40 -0700

    Small cleanup to the UI

--- ui/src/app/services/backend.service.ts
+++ ui/src/app/services/backend.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { GeneralizationResult } from '../interfaces.ts';
+import { GeneralizationResult } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
