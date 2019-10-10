ce4c0e5f7e727eb59bfe54acef975250ccb4f7c9
Samer Albahra <salbahra@gmail.com>
2019-10-10 09:20:56 -0700

    Update labels for reliability curve

--- ui/src/app/components/roc-chart/roc-chart.component.ts
+++ ui/src/app/components/roc-chart/roc-chart.component.ts
@@ -72,7 +72,7 @@ export class RocChartComponent implements OnInit, OnChanges {
             .attr('y', 40)
             .style('font-size', '12px')
             .style('text-anchor', 'middle')
-            .text(this.mode === 'reliability' ? 'Mean Predicted Value' : 'False Positive Rate');
+            .text(this.mode === 'reliability' ? 'Predicted Probability' : 'False Positive Rate');
 
 
         this.svg.append('g')
@@ -84,7 +84,7 @@ export class RocChartComponent implements OnInit, OnChanges {
             .attr('x', 0 - this.cfg.height / 2.8)
             .style('font-size', '12px')
             .style('text-anchor', 'left')
-            .text(this.mode === 'reliability' ? 'Fraction of Positives' : 'True Positive Rate');
+            .text(this.mode === 'reliability' ? 'Observed Probability' : 'True Positive Rate');
 
         this.svg.append('line')
             .attr('class', 'curve')
