d4f39e5543c5ee572f2ad05d30cae04f84fa3ca6
Samer Albahra <salbahra@gmail.com>
2019-12-08 13:46:11 -0800

    Save parameters to the metadata JSON

--- server.py
+++ server.py
@@ -26,7 +26,7 @@ CELERY = Celery(APP.name, backend='rpc://', broker='pyamqp://guest@localhost//')
 CELERY.conf.update(task_track_started=True)
 
 @CELERY.task()
-def queue_training(userid, jobid, form):
+def queue_training(userid, jobid, parameters):
     folder = 'data/' + userid + '/' + jobid
 
     label = open(folder + '/label.txt', 'r')
@@ -35,13 +35,23 @@ def queue_training(userid, jobid, form):
 
     labels = ['No ' + label_column, label_column]
 
-    os.environ['IGNORE_ESTIMATOR'] = form['ignore_estimator']
-    os.environ['IGNORE_FEATURE_SELECTOR'] = form['ignore_feature_selector']
-    os.environ['IGNORE_SCALER'] = form['ignore_scaler']
-    os.environ['IGNORE_SEARCHER'] = form['ignore_searcher']
-    os.environ['IGNORE_SCORER'] = form['ignore_scorer']
-    if form.get('ignore_shuffle'):
-        os.environ['IGNORE_SHUFFLE'] = form.get('ignore_shuffle')
+    os.environ['IGNORE_ESTIMATOR'] = parameters['ignore_estimator']
+    os.environ['IGNORE_FEATURE_SELECTOR'] = parameters['ignore_feature_selector']
+    os.environ['IGNORE_SCALER'] = parameters['ignore_scaler']
+    os.environ['IGNORE_SEARCHER'] = parameters['ignore_searcher']
+    os.environ['IGNORE_SCORER'] = parameters['ignore_scorer']
+    if 'ignore_shuffle' in parameters:
+        os.environ['IGNORE_SHUFFLE'] = parameters['ignore_shuffle']
+
+    metadata = {}
+    if os.path.exists(folder + '/metadata.json'):
+        with open(folder + '/metadata.json') as metafile:
+            metadata = json.load(metafile)
+
+    metadata['parameters'] = parameters
+
+    with open(folder + '/metadata.json', 'w') as metafile:
+        json.dump(metadata, metafile)
 
     api.find_best_model(folder + '/train.csv', folder + '/test.csv', labels, label_column, folder)
     return {}
