3c27eebb5f959f33b28c781c324665cf04172bda
Samer Albahra <salbahra@gmail.com>
2020-01-04 15:02:37 -0800

    Add robust scaler support

--- ml/processors/scalers.py
+++ ml/processors/scalers.py
@@ -2,16 +2,18 @@
 All scalers used
 """
 
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.preprocessing import MinMaxScaler, RobustScaler, StandardScaler
 
 SCALERS = {
     'none': '',
     'minmax': MinMaxScaler(),
+    'robust': RobustScaler(),
     'std': StandardScaler()
 }
 
 SCALER_NAMES = {
     'none': 'no scaling',
     'minmax': 'min max scaler',
+    'robust': 'robust scaler',
     'std': 'standard scaler'
 }
