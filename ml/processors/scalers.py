c864f06bd89fe9d464778a0cb3f813e17e5e8621
Samer Albahra <salbahra@gmail.com>
2020-01-04 17:35:40 -0800

    Remove robust scaler

--- ml/processors/scalers.py
+++ ml/processors/scalers.py
@@ -7,13 +7,11 @@ from sklearn.preprocessing import MinMaxScaler, RobustScaler, StandardScaler
 SCALERS = {
     'none': '',
     'minmax': MinMaxScaler(),
-    'robust': RobustScaler(),
     'std': StandardScaler()
 }
 
 SCALER_NAMES = {
     'none': 'no scaling',
     'minmax': 'min max scaler',
-    'robust': 'robust scaler',
     'std': 'standard scaler'
 }
