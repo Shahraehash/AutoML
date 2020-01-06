0f390e54dcea5858a5d75910d3ccb024cb4939c5
Samer Albahra <salbahra@gmail.com>
2020-01-05 20:19:18 -0800

    Export ROC graphs when using the export button

--- ui/src/app/pages/home/home.page.ts
+++ ui/src/app/pages/home/home.page.ts
@@ -56,6 +56,36 @@ export class HomePage implements OnInit {
 
   exportCSV() {
     window.open(this.backend.exportCSV(), '_self');
+    let rocCurves = `
+    <style>
+      .roc {width: 100%}
+      .roc .axis {fill: #222428}
+      .roc .axis path, .roc .axis line {
+        fill: none;
+        stroke: grey;
+        stroke-width: 2;
+        shape-rendering: crispEdges;
+        opacity: 1
+      }
+      .roc .guess {stroke: #f04141}
+      .roc .ideal {stroke: #10dc60}
+      .roc path {
+        stroke-width: 3;
+        fill: none;
+        opacity: .7;
+      }
+      .roc text {text-transform: capitalize}
+    </style>
+    `;
+    document.querySelectorAll('.roc').forEach(ele => rocCurves += ele.outerHTML);
+    const ifr = document.createElement('iframe');
+    ifr.style.height = '0';
+    ifr.style.width = '0'
+    ifr.style.position = 'absolute';
+    document.body.appendChild(ifr);
+    ifr.contentDocument.body.innerHTML = rocCurves;
+    ifr.contentWindow.print();
+    setTimeout(() => ifr.parentElement.removeChild(ifr), 100);
   }
 
   reset() {
