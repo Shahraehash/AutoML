b8bd946cf4d67c9b42ec0d3a0c7d021d0709c20f
Samer Albahra <salbahra@gmail.com>
2020-01-22 19:01:20 -0800

    Fix more unit tests

--- ui/src/app/pages/login/login.page.spec.ts
+++ ui/src/app/pages/login/login.page.spec.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterModule } from '@angular/router';
@@ -12,6 +13,7 @@ describe('LoginPage', () => {
     TestBed.configureTestingModule({
       declarations: [ LoginPage ],
       imports: [
+        CommonModule,
         RouterModule
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
