fd008b2afd33c29e53c22ad4d752e140693dca46
Samer Albahra <salbahra@gmail.com>
2020-03-05 10:07:34 -0800

    Update unit tests for rounding change

--- ml/unit_test.py
+++ ml/unit_test.py
@@ -22,9 +22,9 @@ def test_logistic_regression():
     model = generate_model(pipeline[0], FEATURE_NAMES, X_TRAIN, Y_TRAIN)
     model.update(refit_model(pipeline[0], model['features'], 'lr', 'accuracy', X_TRAIN, Y_TRAIN))
     generalization = generalize(model, pipeline[0], X2, Y2)
-    assert generalization['accuracy'] == 0.37254901960784315
+    assert generalization['accuracy'] == 0.3725
     assert generalization['avg_sn_sp'] == 0.6
-    assert generalization['f1'] == 0.3703703703703704
+    assert generalization['f1'] == 0.3704
     assert generalization['sensitivity'] == 1.0
     assert generalization['specificity'] == 0.2
 
@@ -35,9 +35,9 @@ def test_logistic_regression_with_standard_scaler():
     model = generate_model(pipeline[0], FEATURE_NAMES, X_TRAIN, Y_TRAIN)
     model.update(refit_model(pipeline[0], model['features'], 'lr', 'accuracy', X_TRAIN, Y_TRAIN))
     generalization = generalize(model, pipeline[0], X2, Y2)
-    assert generalization['accuracy'] == 0.49019607843137253
+    assert generalization['accuracy'] == 0.4902
     assert generalization['avg_sn_sp'] == 0.675
-    assert generalization['f1'] == 0.48842592592592593
+    assert generalization['f1'] == 0.4884
     assert generalization['sensitivity'] == 1.0
     assert generalization['specificity'] == 0.35
 
@@ -48,9 +48,9 @@ def test_logistic_regression_with_standard_scaler_with_select_75():
     model = generate_model(pipeline[0], FEATURE_NAMES, X_TRAIN, Y_TRAIN)
     model.update(refit_model(pipeline[0], model['features'], 'lr', 'accuracy', X_TRAIN, Y_TRAIN))
     generalization = generalize(model, pipeline[0], X2, Y2)
-    assert generalization['accuracy'] == 0.35294117647058826
+    assert generalization['accuracy'] == 0.3529
     assert generalization['avg_sn_sp'] == 0.5875
-    assert generalization['f1'] == 0.34893617021276596
+    assert generalization['f1'] == 0.3489
     assert generalization['sensitivity'] == 1.0
     assert generalization['specificity'] == 0.175
 
@@ -61,9 +61,9 @@ def test_k_nearest_neighbor():
     model = generate_model(pipeline[0], FEATURE_NAMES, X_TRAIN, Y_TRAIN)
     model.update(refit_model(pipeline[0], model['features'], 'knn', 'accuracy', X_TRAIN, Y_TRAIN))
     generalization = generalize(model, pipeline[0], X2, Y2)
-    assert generalization['accuracy'] == 0.39215686274509803
+    assert generalization['accuracy'] == 0.3922
     assert generalization['avg_sn_sp'] == 0.6125
-    assert generalization['f1'] == 0.39122063919907585
+    assert generalization['f1'] == 0.3912
     assert generalization['sensitivity'] == 1.0
     assert generalization['specificity'] == 0.225
 
@@ -74,10 +74,10 @@ def test_k_nearest_neighbor_with_standard_scaler():
     model = generate_model(pipeline[0], FEATURE_NAMES, X_TRAIN, Y_TRAIN)
     model.update(refit_model(pipeline[0], model['features'], 'knn', 'accuracy', X_TRAIN, Y_TRAIN))
     generalization = generalize(model, pipeline[0], X2, Y2)
-    assert generalization['accuracy'] == 0.5490196078431373
-    assert generalization['avg_sn_sp'] == 0.6795454545454545
-    assert generalization['f1'] == 0.5376428852975955
-    assert generalization['sensitivity'] == 0.9090909090909091
+    assert generalization['accuracy'] == 0.5490
+    assert generalization['avg_sn_sp'] == 0.6795
+    assert generalization['f1'] == 0.5376
+    assert generalization['sensitivity'] == 0.9091
     assert generalization['specificity'] == 0.45
 
 def test_k_nearest_neighbor_with_standard_scaler_with_select_75():
@@ -87,9 +87,9 @@ def test_k_nearest_neighbor_with_standard_scaler_with_select_75():
     model = generate_model(pipeline[0], FEATURE_NAMES, X_TRAIN, Y_TRAIN)
     model.update(refit_model(pipeline[0], model['features'], 'knn', 'accuracy', X_TRAIN, Y_TRAIN))
     generalization = generalize(model, pipeline[0], X2, Y2)
-    assert generalization['accuracy'] == 0.49019607843137253
+    assert generalization['accuracy'] == 0.4902
     assert generalization['avg_sn_sp'] == 0.675
-    assert generalization['f1'] == 0.48842592592592593
+    assert generalization['f1'] == 0.4884
     assert generalization['sensitivity'] == 1.0
     assert generalization['specificity'] == 0.35
 
@@ -100,10 +100,10 @@ def test_support_vector_machine():
     model = generate_model(pipeline[0], FEATURE_NAMES, X_TRAIN, Y_TRAIN)
     model.update(refit_model(pipeline[0], model['features'], 'svm', 'accuracy', X_TRAIN, Y_TRAIN))
     generalization = generalize(model, pipeline[0], X2, Y2)
-    assert generalization['accuracy'] == 0.2549019607843137
-    assert generalization['avg_sn_sp'] == 0.4920454545454545
-    assert generalization['f1'] == 0.24059561128526644
-    assert generalization['sensitivity'] == 0.9090909090909091
+    assert generalization['accuracy'] == 0.2549
+    assert generalization['avg_sn_sp'] == 0.4920
+    assert generalization['f1'] == 0.2406
+    assert generalization['sensitivity'] == 0.9091
     assert generalization['specificity'] == 0.075
 
 def test_support_vector_machine_with_standard_scaler():
@@ -113,9 +113,9 @@ def test_support_vector_machine_with_standard_scaler():
     model = generate_model(pipeline[0], FEATURE_NAMES, X_TRAIN, Y_TRAIN)
     model.update(refit_model(pipeline[0], model['features'], 'svm', 'accuracy', X_TRAIN, Y_TRAIN))
     generalization = generalize(model, pipeline[0], X2, Y2)
-    assert generalization['accuracy'] == 0.4117647058823529
+    assert generalization['accuracy'] == 0.4118
     assert generalization['avg_sn_sp'] == 0.625
-    assert generalization['f1'] == 0.4115384615384615
+    assert generalization['f1'] == 0.4115
     assert generalization['sensitivity'] == 1.0
     assert generalization['specificity'] == 0.25
 
@@ -126,10 +126,10 @@ def test_support_vector_machine_with_standard_scaler_and_roc_auc():
     model = generate_model(pipeline[0], FEATURE_NAMES, X_TRAIN, Y_TRAIN)
     model.update(refit_model(pipeline[0], model['features'], 'svm', 'roc_auc', X_TRAIN, Y_TRAIN))
     generalization = generalize(model, pipeline[0], X2, Y2)
-    assert generalization['accuracy'] == 0.7058823529411765
-    assert generalization['avg_sn_sp'] == 0.6806818181818182
-    assert generalization['f1'] == 0.6386395843174303
-    assert generalization['sensitivity'] == 0.6363636363636364
+    assert generalization['accuracy'] == 0.7059
+    assert generalization['avg_sn_sp'] == 0.6807
+    assert generalization['f1'] == 0.6386
+    assert generalization['sensitivity'] == 0.6364
     assert generalization['specificity'] == 0.725
 
 def test_support_vector_machine_with_standard_scaler_with_select_75():
@@ -139,8 +139,8 @@ def test_support_vector_machine_with_standard_scaler_with_select_75():
     model = generate_model(pipeline[0], FEATURE_NAMES, X_TRAIN, Y_TRAIN)
     model.update(refit_model(pipeline[0], model['features'], 'svm', 'accuracy', X_TRAIN, Y_TRAIN))
     generalization = generalize(model, pipeline[0], X2, Y2)
-    assert generalization['accuracy'] == 0.23529411764705882
+    assert generalization['accuracy'] == 0.2353
     assert generalization['avg_sn_sp'] == 0.5125
-    assert generalization['f1'] == 0.20471811275489804
+    assert generalization['f1'] == 0.2047
     assert generalization['sensitivity'] == 1.0
     assert generalization['specificity'] == 0.025
