64a97f10f7f4dcee49c3cf6e391bd8374c5a99e2
Samer Albahra <salbahra@gmail.com>
2021-06-12 18:55:16 -0700

    Link scrolling

--- ui/src/app/components/feature-details/feature-details.component.ts
+++ ui/src/app/components/feature-details/feature-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { IonContent } from '@ionic/angular';
 
 import { DataAnalysis } from '../../interfaces';
 
@@ -11,10 +12,19 @@ export class FeatureDetailsComponent implements OnInit {
   @Input() data: DataAnalysis;
   @Input() label: string;
   @Input() type: string;
-
+  @Output() contentScroll = new EventEmitter();
+  
   features: string[];
 
+  @ViewChild('content') private content: IonContent;
+  
   ngOnInit() {
     this.features = Object.keys(this.data.summary).filter(item => item !== this.label);
+  }
+
+  updateScroll(position: number) {
+    if (this.content && typeof position === 'number') {
+      this.content.scrollToPoint(0, position, 0);
+    }
   }
 }
