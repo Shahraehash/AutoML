1bc5da789125fbae5bf622b7ce31b92ebbeab794
Samer Albahra <salbahra@gmail.com>
2021-09-20 15:23:17 -0700

    Use more efficient method

--- ml/utils.py
+++ ml/utils.py
@@ -2,7 +2,7 @@
 Utilities
 """
 
-from rdp import rdp
+import numpy as np
 
 from .processors.estimators import ESTIMATOR_NAMES
 from .processors.feature_selection import FEATURE_SELECTOR_NAMES
@@ -30,3 +30,30 @@ def decimate_points(x, y):
     """Removes unneeded points from a curve"""
 
     return list(zip(*rdp(list(zip(x, y)))))
+
+def line_dists(points, start, end):
+    if np.all(start == end):
+        return np.linalg.norm(points - start, axis=1)
+
+    vec = end - start
+    cross = np.cross(vec, start - points)
+    return np.divide(abs(cross), np.linalg.norm(vec))
+
+
+def rdp(M, epsilon=0):
+    M = np.array(M)
+    start, end = M[0], M[-1]
+    dists = line_dists(M, start, end)
+
+    index = np.argmax(dists)
+    dmax = dists[index]
+
+    if dmax > epsilon:
+        result1 = rdp(M[:index + 1], epsilon)
+        result2 = rdp(M[index:], epsilon)
+
+        result = np.vstack((result1[:-1], result2))
+    else:
+        result = np.array([start, end])
+
+    return result
