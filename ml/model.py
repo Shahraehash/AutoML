c91cc30c219a75067fa1585f162f322fe358a755
Samer Albahra <salbahra@gmail.com>
2021-12-02 15:05:24 -0800

    Add feature scores for select percentile and RFFI

--- ml/model.py
+++ ml/model.py
@@ -13,6 +13,8 @@ def generate_model(pipeline, feature_names, x_train, y_train):
 
     start = timer()
     features = {}
+    feature_scores = None
+    selected_features = feature_names
 
     pipeline.fit(x_train, y_train)
 
@@ -20,6 +22,9 @@ def generate_model(pipeline, feature_names, x_train, y_train):
         feature_selector_type = pipeline.named_steps['feature_selector'].__class__.__module__
 
         if 'sklearn.feature_selection.univariate_selection' in feature_selector_type:
+            feature_scores = pipeline.named_steps['feature_selector'].scores_
+            feature_scores = pd.DataFrame({'scores': feature_scores, 'selected': pipeline.named_steps['feature_selector'].get_support()}, index=feature_names)
+            feature_scores = feature_scores[feature_scores['selected'] == True].drop(columns=['selected'])
             features = pd.Series(pipeline.named_steps['feature_selector'].get_support(),
                                  index=feature_names)
             selected_features = features[features == True].axes[0]
@@ -28,13 +33,17 @@ def generate_model(pipeline, feature_names, x_train, y_train):
             most_important = pipeline.named_steps['feature_selector'].get_top_features()
             most_important_names =\
                 [feature_names[most_important[i]] for i in range(len(most_important))]
+            feature_scores = pipeline.named_steps['feature_selector'].model.feature_importances_
+            feature_scores = pd.DataFrame({'scores': feature_scores, 'selected': (i in most_important_names for i in feature_names)}, index=feature_names)
+            feature_scores = feature_scores[feature_scores['selected'] == True].drop(columns=['selected'])
             features = pd.Series((i in most_important_names for i in feature_names),
                                  index=feature_names)
             selected_features = features[features == True].axes[0]
-        else:
-            selected_features = feature_names
-    else:
-        selected_features = feature_names
+
+    if feature_scores is not None:
+        total_score = feature_scores['scores'].sum()
+        feature_scores['scores'] = round(feature_scores['scores'] / total_score, 4)
+        feature_scores = dict(feature_scores['scores'])
 
     print('\tFeatures used: ' + ', '.join(selected_features[:MAX_FEATURES_SHOWN]) +
           ('...' if len(selected_features) > MAX_FEATURES_SHOWN else ''))
@@ -44,5 +53,6 @@ def generate_model(pipeline, feature_names, x_train, y_train):
 
     return {
         'features': features,
-        'selected_features': selected_features
+        'selected_features': selected_features,
+        'feature_scores': feature_scores
     }
