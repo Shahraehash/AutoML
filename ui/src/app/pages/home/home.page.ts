dbcbaf1fca760cc45e4c345ea406633255a9fd17
Samer Albahra <salbahra@gmail.com>
2022-01-23 17:58:07 -0800

    Update home page to require license

--- ui/src/app/pages/home/home.page.ts
+++ ui/src/app/pages/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
@@ -6,21 +6,36 @@ import { AlertController } from '@ionic/angular';
 import { environment } from '../../../environments/environment';
 import { version } from '../../../../../package.json';
 import { MiloApiService } from '../../services';
+import { takeUntil } from 'rxjs/operators';
+import { ReplaySubject } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit, OnDestroy {
   version = version;
   license = new FormControl('', [Validators.required]);
+  destroy$ = new ReplaySubject<boolean>();
 
   constructor(
     public api: MiloApiService,
     private router: Router,
     private alertController: AlertController
   ) {}
+
+  ngOnInit() {
+    this.api.events.pipe(takeUntil(this.destroy$)).subscribe(event => {
+      if (event === 'license_error') {
+        this.router.navigate(['update-license']);
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+  }
 
   get isDocker() {
     return environment.name === 'docker';
